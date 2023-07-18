49f4c55ae451ad63d389db34b9b38dbf896a3480
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2023-07-18 23:37:29 +0900

    Add files via upload

---
+++ permutations/next_permutation.py
@@ -0,0 +1,35 @@
+def next_permutations(nums):
+    i = len(nums) - 1
+    while i > 0 and nums[i - 1] >= nums[i]:
+        i -= 1
+
+    if i == 0:
+        nums.sort()
+        return
+
+    j = len(nums) - 1
+    while nums[j] <= nums[i-1]:
+        j -= 1
+
+    nums[i-1], nums[j] = nums[j], nums[i-1]
+    nums[i:] = reversed(nums[i:])
+    return nums
+
+def prev_permutations(nums):
+    i = len(nums) - 1
+    while i > 0 and nums[i - 1] <= nums[i]:
+        i -= 1
+
+    if i == 0:
+        nums.sort(reverse=True)
+        return
+
+    j = len(nums) - 1
+    while nums[j] >= nums[i-1]:
+        j -= 1
+
+    nums[i-1], nums[j] = nums[j], nums[i-1]
+    nums[i:] = reversed(nums[i:])
+    return nums
+
+print(prev_permutations([1, 3, 5, 4, 2]))
