9b6d2124a0e51352f10255435b90d4e814c54d8c
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2023-08-13 23:55:42 +0900

    Add files via upload

---
+++ 9000~9999/9316.py
@@ -0,0 +1,12 @@
+# 당신은 N개의 테스트케이스들에게 반드시 인사를 해야 이 문제를 풀 수 있다.
+#
+# N개의 줄에 걸쳐
+#
+# "Hello World, Judge i!"
+#
+# 를 출력하는 프로그램을 만들라. 여기서 i는 줄의 번호이다.
+
+N = int(input())
+
+for i in range(1, N+1):
+    print("Hello World, Judge " + str(i) + "!")
---
+++ 9000~9999/9654.py
@@ -0,0 +1,8 @@
+# 나부 행성의 함대 정보를 출력하는 프로그램을 작성하시오.
+
+print('SHIP NAME      CLASS          DEPLOYMENT IN SERVICE')
+print('N2 Bomber      Heavy Fighter  Limited    21        ')
+print('J-Type 327     Light Combat   Unlimited  1         ')
+print('NX Cruiser     Medium Fighter Limited    18        ')
+print('N1 Starfighter Medium Fighter Unlimited  25        ')
+print('Royal Cruiser  Light Combat   Limited    4         ')
